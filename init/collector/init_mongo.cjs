b6148f62e0fcf7698d3e18b056b884ae500afb5d
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-04-27 10:45:22 +0200

    refactor: Clean code

--- init/collector/init_mongo.cjs
+++ init/collector/init_mongo.cjs
@@ -1,14 +1,11 @@
-// Create a new database and switch to it
 db = db.getSiblingDB('mydatabase');
 
-// Create a new collection and insert documents
 db.mycollection.insert([
   { name: 'Document 1' },
   { name: 'Document 2' },
   { name: 'Document 3' }
 ]);
 
-// Create a user with read and write privileges for the database
 db.createUser({
   user: 'myuser',
   pwd: 'mypassword',
